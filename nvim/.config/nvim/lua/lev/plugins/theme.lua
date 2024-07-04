1789789ef853629a38e9d3c40db25f1d1bd93805
Lev Guzman <levguzman@levs-mbp.mynetworksettings.com>
2024-07-04 08:25:32 -0400

    change gutter number coloring

--- nvim/.config/nvim/lua/lev/plugins/theme.lua
+++ nvim/.config/nvim/lua/lev/plugins/theme.lua
@@ -5,7 +5,11 @@ return {
 		require("tokyonight").setup({
 			style = "storm",
 		})
-
+		local number_color = "#bb9af7"
+    local number_line_color = "#CEB25E"
 		vim.cmd("colorscheme tokyonight")
+		vim.api.nvim_set_hl(0, "LineNrAbove", { fg = number_color, bold = true })
+		vim.api.nvim_set_hl(0, "LineNr", { fg = number_line_color, bold = true })
+		vim.api.nvim_set_hl(0, "LineNrBelow", { fg = number_color, bold = true })
 	end,
 }
