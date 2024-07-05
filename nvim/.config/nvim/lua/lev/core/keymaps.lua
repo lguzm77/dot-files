d85feab56271e0a8e5be60f1a74706c9302f66f8
Lev Guzman <levguzman@levs-mbp.mynetworksettings.com>
2024-07-04 21:54:56 -0400

    add leader p remap and rename opts variable

--- nvim/.config/nvim/lua/lev/core/keymaps.lua
+++ nvim/.config/nvim/lua/lev/core/keymaps.lua
@@ -1,14 +1,12 @@
 local keymap = vim.keymap
 
-local opts = {
+local non_recursive_option = {
 	noremap = true, -- non-recursive
 }
 
+-- greatest remap ever, search and replace all occurrences of the current buffer with your register's content
+keymap.set("v","<leader>p", "\"_dP", non_recursive_option)
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
------------------
--- Normal mode --
------------------
-
 
 -- window management
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
@@ -22,31 +20,26 @@ keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
--- stop highlighting when searching
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
 -- TODO: Add descriptions to each command
 --
 -- Resize with arrows
 -- delta: 2 lines
-keymap.set("n", "<C-Up>", ":resize -2<CR>", opts)
-keymap.set("n", "<C-Down>", ":resize +2<CR>", opts)
-keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", opts)
-
------------------
--- Visual mode --
------------------
+keymap.set("n", "<C-Up>", ":resize -2<CR>", non_recursive_option)
+keymap.set("n", "<C-Down>", ":resize +2<CR>", non_recursive_option)
+keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", non_recursive_option)
+keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", non_recursive_option)
 
 -- Hint: start visual mode with the same area as the previous area and the same mode
-keymap.set("v", "<", "<gv", opts)
-keymap.set("v", ">", ">gv", opts)
+keymap.set("v", "<", "<gv", non_recursive_option)
+keymap.set("v", ">", ">gv", non_recursive_option)
 
 -- center cursor when moving vertically
 -- Navigation and centering is handled by cinnamon.lua
-keymap.set("n", "n", "nzzzv", opts)
-keymap.set("n", "N", "Nzzzv", opts)
-vim.api.nvim_set_keymap("n", "{", "{zz", opts)
-vim.api.nvim_set_keymap("n", "<C-d>", "<C-d>zz", opts) -- zz centers your cursor
-vim.api.nvim_set_keymap("n", "<C-u>", "<C-u>zz", opts)
-vim.api.nvim_set_keymap("n", "}", "}zz", opts)
+keymap.set("n", "n", "nzzzv", non_recursive_option)
+keymap.set("n", "N", "Nzzzv", non_recursive_option)
+vim.api.nvim_set_keymap("n", "{", "{zz", non_recursive_option)
+vim.api.nvim_set_keymap("n", "<C-d>", "<C-d>zz", non_recursive_option) -- zz centers your cursor
+vim.api.nvim_set_keymap("n", "<C-u>", "<C-u>zz", non_recursive_option)
+vim.api.nvim_set_keymap("n", "}", "}zz", non_recursive_option)
