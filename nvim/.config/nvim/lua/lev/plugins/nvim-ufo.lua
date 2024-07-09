a5610e8dcd25efdb98e686b8478c7e2df6aedd1b
Lev Guzman <levguzman@levs-mbp.mynetworksettings.com>
2024-07-09 07:24:16 -0400

    tweak nvim ufo

--- nvim/.config/nvim/lua/lev/plugins/nvim-ufo.lua
+++ nvim/.config/nvim/lua/lev/plugins/nvim-ufo.lua
@@ -2,12 +2,24 @@ return {
 	"kevinhwang91/nvim-ufo",
 	dependencies = { "kevinhwang91/promise-async" },
 	config = function()
-		require("ufo").setup({
-			provider_selector = function(bufnr, filetype, buftype)
-				return { "treesitter", "indent" }
-			end,
-		})
+		vim.o.foldcolumn = "0"
+		vim.o.foldlevel = 99
+		vim.o.foldlevelstart = 99
+		vim.o.foldenable = true
+
 		vim.keymap.set("n", "zR", require("ufo").openAllFolds)
 		vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+		vim.keymap.set("n", "zK", function()
+			local winid = require("ufo").peekFoldedLinesUnderCursor()
+			if not winid then
+				vim.lsp.buf.hover()
+			end
+		end, { desc = "Peek Fold under cursor" })
+
+		require("ufo").setup({
+			provider_selector = function(bufnr, filetype, buftype)
+				return { "lsp", "indent" }
+			end,
+		})
 	end,
 }
