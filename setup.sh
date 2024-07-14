d9d380a77c0c68cdfb3240baa8fe28dd4ec6ed87
Lev Guzman <levguzman@levs-mbp.mynetworksettings.com>
2024-07-14 12:01:01 -0400

    fix typo in graphvix and add installation for diagrams and mermaid

--- setup.sh
+++ setup.sh
@@ -33,7 +33,7 @@ function installUsingHomebrew(){
 }
 
 echo "Installing development tools using homebrew"
-development_tools=("minikube elixir neovim docker kubectl fnm graphiz python") # fnm stands for fast node manager
+development_tools=("minikube elixir neovim docker kubectl fnm graphviz python") # fnm stands for fast node manager
 installUsingHomebrew "${development_tools[@]}"
   
 echo "Installing shell tools using homebrew"
@@ -46,6 +46,49 @@ for module in $(ls -d */)/;
 do 
   stow $module;
 done
+
+echo "Finished installing tools, setting up home directory folders and tools"
+cd $HOME
+
+# TODO: can you add colors to the echo lines?
+echo "Setting up Python environment for diagrams"
+
+function installDiagrams(){
+mkdir architecture-diagrams
+
+
+# Setup a new python3 virtual environment
+cd ./architecture-diagrams && python3 -m venv diagrams-pyenv
+
+# Install all necessary tooling
+source diagrams-pyenv/bin/activate
+pip3 install diagrams
+
+#return to the root directory
+cd ..
+}
+
+# CHeck if the directory exists
+if [ ! -d "architecture-diagrams" ]; then
+  installDiagrams
+  echo "architecture-diagrams directory created and diagram tooling installed"
+else
+  # if the directory exists, we can assume that the necessary tooling is already installed. 
+  # This script is meant to run in a new computer
+  echo "Directory architecture-diagrams already exists"
+fi
+
+
+echo "Setting up Mermaid diagrams local environment"
+
+if ! command -v mmdc >&2; then
+  npm install -g @mermaid-js/mermaid-cli
+  mkdir mermaid-diagrams
+  echp "Mermaid tooling installed and mermaid-diagrams directory created"
+else
+  echo "Mermaid is already installed"
+fi
+# Usage mmdc -i input.mmd -o output.png -t dark -b transparent
 
 echo "Finished setup script"
 
