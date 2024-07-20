218eeae86265aee8b7076975d8a87d49ba44b4c3
Lev Guzman <levguzman@Levs-MacBook-Pro.local>
2024-07-19 20:26:09 -0400

    Fix GBrowse command
    oil.nvim disables newtr so a custom implementation of Browse must be
    provided

--- nvim/.config/nvim/lua/lev/plugins/git-stuff.lua
+++ nvim/.config/nvim/lua/lev/plugins/git-stuff.lua
@@ -1,41 +1,48 @@
 return {
-  {
-    "kdheepak/lazygit.nvim",
-    cmd = {
-      "LazyGit",
-      "LazyGitConfig",
-      "LazyGitCurrentFile",
-      "LazyGitFilter",
-      "LazyGitFilterCurrentFile",
-    },
-    -- optional for floating window border decoration
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    -- setting the keybinding for LazyGit with 'keys' is recommended in
-    -- order to load the plugin when the command is run for the first time
-    keys = {
-      { "<leader>gl", "<cmd>LazyGit<cr>", desc = "Open lazy git" },
-    },
-  },
-  {
-    "pwntester/octo.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-      "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-      require("octo").setup({ enable_builtin = true })
-      vim.cmd([[hi OctoEditable guibg=none]]) -- removes background colors in diff showing
-    end,
-    keys = { { "<leader>O", "<cmd>Octo<cr>", desc = "Octo" } },
-  },
-  {
-    "tpope/vim-fugitive",
-    dependencies = { "tpope/vim-rhubarb" },
-    config = function()
-      vim.keymap.set({ "n", "v" }, "<leader>op", ":GBrowse<CR>", {})
-    end,
-  },
+	{
+		"kdheepak/lazygit.nvim",
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		-- optional for floating window border decoration
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		-- setting the keybinding for LazyGit with 'keys' is recommended in
+		-- order to load the plugin when the command is run for the first time
+		keys = {
+			{ "<leader>gl", "<cmd>LazyGit<cr>", desc = "Open lazy git" },
+		},
+	},
+	{
+		"pwntester/octo.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("octo").setup({ enable_builtin = true })
+			vim.cmd([[hi OctoEditable guibg=none]]) -- removes background colors in diff showing
+		end,
+		keys = { { "<leader>O", "<cmd>Octo<cr>", desc = "Octo" } },
+	},
+	{
+		"tpope/vim-fugitive",
+		dependencies = { "tpope/vim-rhubarb" },
+		config = function()
+			-- Custom :Browse command
+			vim.api.nvim_create_user_command("Browse", function(opts)
+				-- xdg-open for linux, open for mac
+        -- TODO: Can you make this function execute the correct command based on the operation system?
+				vim.fn.system({ "open", opts.fargs[1] })
+			end, { nargs = 1 })
+
+			vim.keymap.set({ "n", "v" }, "<leader>op", ":GBrowse<CR>", {})
+		end,
+	},
 }
