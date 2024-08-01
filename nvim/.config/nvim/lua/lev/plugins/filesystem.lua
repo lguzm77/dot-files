a49208a128e1fb5c8ed6819f5ed939a5dc88d5f2
Lev Guzman <levguzman@levs-mbp.mynetworksettings.com>
2024-08-01 19:33:17 -0400

    Lazy load oil.nvim

--- nvim/.config/nvim/lua/lev/plugins/filesystem.lua
+++ nvim/.config/nvim/lua/lev/plugins/filesystem.lua
@@ -2,7 +2,7 @@ return {
 	"stevearc/oil.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	keys = {
-		{ "-", "<cmd>Oil<CR>", { desc = "Open parent directory" } },
+		{ "-", "<cmd>Oil<CR>",  desc = "Open parent directory"  },
 	},
 	config = function()
 		require("oil").setup({
