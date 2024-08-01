f5cff82dcc77596961b1b8cdf20b61cab2b6fa70
Lev Guzman <levguzman@levs-mbp.mynetworksettings.com>
2024-08-01 19:10:41 -0400

    Lazy load oil when pressing a key

--- nvim/.config/nvim/lua/lev/plugins/filesystem.lua
+++ nvim/.config/nvim/lua/lev/plugins/filesystem.lua
@@ -1,6 +1,9 @@
 return {
 	"stevearc/oil.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
+	keys = {
+		{ "-", "<cmd>Oil<CR>", { desc = "Open parent directory" } },
+	},
 	config = function()
 		require("oil").setup({
 			default_file_explorer = true,
@@ -8,8 +11,5 @@ return {
 				show_hidden = true,
 			},
 		})
-
-		local keymap = vim.keymap
-		keymap.set("n", "-", "<cmd>Oil<CR>", { desc = "Open parent directory" })
 	end,
 }
