2e244f3aa7dbc1d28ab72da8cb058438d35f64b3
Lev Guzman <levguzman@levs-mbp.mynetworksettings.com>
2024-08-08 18:54:34 -0400

    change linting keybinding

--- .config/.config/nvim/lua/lev/plugins/linting.lua
+++ .config/.config/nvim/lua/lev/plugins/linting.lua
@@ -20,7 +20,7 @@ return {
 			end,
 		})
 
-		vim.keymap.set("n", "<leader>fc", function()
+		vim.keymap.set("n", "<leader>ll", function()
 			lint.try_lint()
 		end, { desc = "Trigger linting for current file" })
 	end,
