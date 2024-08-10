8cf9c4a4255475da2b7786cd05f9ddfac15311f3
Lev Guzman <levguzman@Levs-MacBook-Pro.local>
2024-08-10 09:19:30 -0400

    tweak theme

--- .config/.config/nvim/lua/lev/plugins/theme.lua
+++ .config/.config/nvim/lua/lev/plugins/theme.lua
@@ -1,12 +1,15 @@
 return {
 	"folke/tokyonight.nvim",
+	lazy = false,
 	priority = 1000,
 	config = function()
 		require("tokyonight").setup({
 			style = "storm",
+			dim_inactive = true,
+			lualine_bold = true,
 		})
 		local number_color = "#7aa2f7"
-    local number_line_color = "#CEB25E"
+		local number_line_color = "#CEB25E"
 		vim.cmd("colorscheme tokyonight")
 		vim.api.nvim_set_hl(0, "LineNrAbove", { fg = number_color, bold = true })
 		vim.api.nvim_set_hl(0, "LineNr", { fg = number_line_color, bold = true })
