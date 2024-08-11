204766da1bd3e05f3c93b2c6e61bc81c0c99d06f
Lev Guzman <levguzman@levs-mbp.mynetworksettings.com>
2024-08-11 18:58:10 -0400

    tweak oil.nvim

--- .config/.config/nvim/lua/lev/plugins/filesystem.lua
+++ .config/.config/nvim/lua/lev/plugins/filesystem.lua
@@ -1,16 +1,23 @@
 return {
 	"stevearc/oil.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-  cmd = "Oil",
+	cmd = "Oil",
 	keys = {
-		{ "-", "<cmd>Oil<CR>",  desc = "Open parent directory"  },
+		{ "-", "<cmd>Oil<CR>", desc = "Open parent directory" },
 	},
 	config = function()
 		require("oil").setup({
 			default_file_explorer = true,
+			delete_to_trash = true,
+			skip_confirm_for_simple_edits = true,
 			view_options = {
 				show_hidden = true,
+				natural_order = true,
+				is_hidden_file = function(name, _)
+					return name == ".." or name == ".git" -- hide .git and .. folders
+				end,
 			},
+			win_options = { wrap = true },
 		})
 	end,
 }
