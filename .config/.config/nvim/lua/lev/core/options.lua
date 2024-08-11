47d66bd995843a0b287843979b917c55f558d990
Lev Guzman <levguzman@levs-mbp.mynetworksettings.com>
2024-08-11 19:17:47 -0400

    add some option

--- .config/.config/nvim/lua/lev/core/options.lua
+++ .config/.config/nvim/lua/lev/core/options.lua
@@ -18,5 +18,11 @@ vim.cmd("cd %:p:h") -- set current directory as base
 vim.cmd("set wrap")
 vim.cmd("set breakindent")
 vim.cmd("set linebreak")
-vim.opt.laststatus = 3 -- lock vim status bar to the bottom
+opt.laststatus = 3 -- lock vim status bar to the bottom
 
+opt.swapfile = false
+opt.backup = false
+opt.signcolumn = "yes"
+opt.scrolloff = 20 -- how many lines to show above and below cursor
+opt.guicursor = ""
+opt.conceallevel = 2
