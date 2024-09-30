2a4219af8eaba0f75a2130f334e9072b75235e69
Lev Guzman <levguzman@levs-mbp.mynetworksettings.com>
2024-09-30 08:36:10 -0400

    Comment out tokyonight

--- nvim/lua/lev/plugins/theme.lua
+++ nvim/lua/lev/plugins/theme.lua
@@ -1,35 +1,32 @@
 return {
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		enable = false,
-		priority = 1000,
-		config = function()
-			require("tokyonight").setup({
-				style = "storm",
-				dim_inactive = true,
-				lualine_bold = true,
-			})
-			local number_color = "#7aa2f7"
-			local number_line_color = "#CEB25E"
-			vim.cmd("colorscheme tokyonight")
-			vim.api.nvim_set_hl(0, "LineNrAbove", { fg = number_color, bold = true })
-			vim.api.nvim_set_hl(0, "LineNr", { fg = number_line_color, bold = true })
-			vim.api.nvim_set_hl(0, "LineNrBelow", { fg = number_color, bold = true })
-		end,
-	},
-
+	-- {
+	-- 	"folke/tokyonight.nvim",
+	-- 	lazy = false,
+	-- 	enable = false,
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		require("tokyonight").setup({
+	-- 			style = "storm",
+	-- 			dim_inactive = true,
+	-- 			lualine_bold = true,
+	-- 		})
+	-- 		local number_color = "#7aa2f7"
+	-- 		local number_line_color = "#CEB25E"
+	-- 		vim.cmd("colorscheme tokyonight")
+	-- 		vim.api.nvim_set_hl(0, "LineNrAbove", { fg = number_color, bold = true })
+	-- 		vim.api.nvim_set_hl(0, "LineNr", { fg = number_line_color, bold = true })
+	-- 		vim.api.nvim_set_hl(0, "LineNrBelow", { fg = number_color, bold = true })
+	-- 	end,
+	-- },
 	{
 		"eldritch-theme/eldritch.nvim",
 		lazy = false,
 		priority = 1000,
 		opts = {},
-    config = function ()
-      vim.cmd("colorscheme eldritch")
-      
-    end
-	}
-,
+		config = function()
+			vim.cmd("colorscheme eldritch")
+		end,
+	},
 	{
 		"nvim-lualine/lualine.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons", "AndreM222/copilot-lualine", "folke/noice.nvim" },
@@ -38,7 +35,7 @@ return {
 			local lualine = require("lualine")
 			lualine.setup({
 				options = {
-					theme = "tokyonight",
+					theme = "eldritch",
 				},
 				sections = {
 					lualine_a = { "mode" },
