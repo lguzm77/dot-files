7e60a5287ced225e1fb1404ee7b751adbe4e544f
Lev Guzman <levguzman@Levs-MacBook-Pro.local>
2024-10-03 11:31:20 -0400

    Use float option in oil.nvim

--- nvim/lua/lev/plugins/filesystem.lua
+++ nvim/lua/lev/plugins/filesystem.lua
@@ -3,13 +3,17 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	cmd = "Oil",
 	keys = {
-		{ "-", "<cmd>Oil<CR>", desc = "Open parent directory" },
+		{ "-", "<cmd>Oil --float<CR>", desc = "Open parent directory" },
 	},
 	config = function()
 		require("oil").setup({
 			default_file_explorer = true,
 			delete_to_trash = true,
 			skip_confirm_for_simple_edits = true,
+			float = {
+				padding = 2,
+				max_width = 90,
+			},
 			view_options = {
 				show_hidden = true,
 				natural_order = true,
