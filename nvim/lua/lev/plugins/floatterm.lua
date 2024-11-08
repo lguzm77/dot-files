9d1df952c37cb9d20c8905aac949cb4e04d42379
lguzm77 <lguzmm77@icloud.com>
2024-11-08 06:41:08 -0500

    Update lazy-lock and tweak floatterm

--- nvim/lua/lev/plugins/floatterm.lua
+++ nvim/lua/lev/plugins/floatterm.lua
@@ -3,12 +3,15 @@ return {
 	keys = {
 		{
 			"<leader>t",
-			"<cmd>:FloatermNew --height=0.7 --width=0.8 --wintype=float --name=floaterm1 --position=center --autoclose=2<CR>",
+			"<cmd>:FloatermNew --height=0.9 --width=0.9 --wintype=float --name=floaterm1 --position=center --autoclose=2<CR>",
 			desc = "Open Floatterm",
 		},
 	},
 	config = function()
-		vim.keymap.set("n", "<leader>flt", "<cmd>:FloatermToggle<CR>", { desc = "Toggle FloatTerm" })
-		vim.keymap.set("t", "<leader>flt", "<cmd>:FloatermToggle<CR>", { desc = "Toggle FloatTerm" })
+		vim.keymap.set("n", "<leader>tt", "<cmd>:FloatermToggle<CR>", { desc = "Toggle FloatTerm" })
+		vim.keymap.set("t", "<leader>tt", "<cmd>:FloatermToggle<CR>", { desc = "Toggle FloatTerm" })
+
+		vim.keymap.set("t", "<leader>n", "<cmd>:FloatermNext<CR>", { desc = "Toggle FloatTerm" })
+		vim.keymap.set("t", "<leader>p", "<cmd>:FloatTermPrev<CR>", { desc = "Toggle FloatTerm" })
 	end,
 }
