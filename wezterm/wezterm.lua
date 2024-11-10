2a41da7ecbfd3520b2ef90a8df879b052c0cf92c
lguzm77 <lguzmm77@icloud.com>
2024-11-10 11:25:02 -0500

    Configure wezterm, add leader and some keybindings

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,13 +1,51 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
+config.font = wezterm.font("Dank Mono")
+config.font_size = 14
+
+config.enable_tab_bar = false
+
+config.window_decorations = "RESIZE"
+
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 10
+
+config.color_scheme = "rose-pine"
+
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Eldritch'
 -- and finally, return the configuration to wezterm
-return config
 
+config.leader = {
+	-- Leader key is ctrl-a
+	key = "a",
+	mods = "CTRL",
+	timeout_milliseconds = 2000,
+}
+
+config.keys = {
+
+	{
+		-- leader+[ leads to copy mode
+		key = "[",
+		mods = "LEADER",
+		action = wezterm.action.ActivateCopyMode,
+	},
+}
+
+local actions = wezterm.action
+config.keys = {
+
+	{
+		key = "c",
+		mods = "LEADER",
+		action = actions.SpawnTab("CurrentPaneDomain"),
+	},
+}
+
+return config
