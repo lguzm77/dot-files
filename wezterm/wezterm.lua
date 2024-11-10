8e5e13bc64c03f9bb6849edc8d4f4610a7280816
lguzm77 <lguzmm77@icloud.com>
2024-11-10 11:32:27 -0500

    Tweak wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,11 +3,12 @@ local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
+local actions = wezterm.action
 
 config.font = wezterm.font("Dank Mono")
 config.font_size = 14
 
-config.enable_tab_bar = false
+config.enable_tab_bar = true
 
 config.window_decorations = "RESIZE"
 
@@ -16,14 +17,9 @@ config.macos_window_background_blur = 10
 
 config.color_scheme = "rose-pine"
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
--- and finally, return the configuration to wezterm
-
 config.leader = {
 	-- Leader key is ctrl-a
-	key = "a",
+	key = "s",
 	mods = "CTRL",
 	timeout_milliseconds = 2000,
 }
@@ -38,13 +34,25 @@ config.keys = {
 	},
 }
 
-local actions = wezterm.action
 config.keys = {
 
 	{
 		key = "c",
 		mods = "LEADER",
 		action = actions.SpawnTab("CurrentPaneDomain"),
+	},
+}
+
+config.keys = {
+	{
+		key = "n",
+		mods = "LEADER",
+		action = actions.ActivateTabRelative(1),
+	},
+	{
+		key = "p",
+		mods = "LEADER",
+		action = actions.ActivateTabRelative(-1),
 	},
 }
 
