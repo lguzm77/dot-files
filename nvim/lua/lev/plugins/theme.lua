fa609ad7eb6cfe73b71f3e51057bcf7d7e5ea2c0
lguzm77 <lguzmm77@icloud.com>
2024-11-10 11:54:21 -0500

    Change theme to pine-rose

--- nvim/lua/lev/plugins/theme.lua
+++ nvim/lua/lev/plugins/theme.lua
@@ -18,13 +18,71 @@ return {
 	-- 		vim.api.nvim_set_hl(0, "LineNrBelow", { fg = number_color, bold = true })
 	-- 	end,
 	-- },
+	-- {
+	-- 	"eldritch-theme/eldritch.nvim",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	opts = {},
+	-- 	config = function()
+	-- 		vim.cmd("colorscheme eldritch")
+	-- 	end,
+	-- },
+
 	{
-		"eldritch-theme/eldritch.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {},
+		"rose-pine/neovim",
+		name = "rose-pine",
 		config = function()
-			vim.cmd("colorscheme eldritch")
+			require("rose-pine").setup({
+				variant = "main", -- auto, main, moon, or dawn
+				dark_variant = "main", -- main, moon, or dawn
+				dim_inactive_windows = true,
+				extend_background_behind_borders = true,
+
+				enable = {
+					terminal = true,
+					legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+					migrations = true, -- Handle deprecated options automatically
+				},
+
+				styles = {
+					bold = true,
+					italic = true,
+					transparency = true,
+				},
+
+				groups = {
+					border = "muted",
+					link = "iris",
+					panel = "surface",
+
+					error = "love",
+					hint = "iris",
+					info = "foam",
+					note = "pine",
+					todo = "rose",
+					warn = "gold",
+
+					git_add = "foam",
+					git_change = "rose",
+					git_delete = "love",
+					git_dirty = "rose",
+					git_ignore = "muted",
+					git_merge = "iris",
+					git_rename = "pine",
+					git_stage = "iris",
+					git_text = "rose",
+					git_untracked = "subtle",
+
+					h1 = "iris",
+					h2 = "foam",
+					h3 = "rose",
+					h4 = "gold",
+					h5 = "pine",
+					h6 = "foam",
+				},
+			})
+
+			vim.cmd("colorscheme rose-pine")
 		end,
 	},
 	{
@@ -35,7 +93,7 @@ return {
 			local lualine = require("lualine")
 			lualine.setup({
 				options = {
-					theme = "eldritch",
+					theme = "rose-pine",
 				},
 				sections = {
 					lualine_a = { "mode" },
