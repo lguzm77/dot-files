ea7cc756a3f8c5fe7d9614aa856c16dea9ecf34c
lguzm77 <lguzmm77@icloud.com>
2024-11-13 07:03:41 -0500

    Add luasnip snippets

--- nvim/lua/lev/plugins/completions.lua
+++ nvim/lua/lev/plugins/completions.lua
@@ -1,5 +1,40 @@
 return {
-  -- TODO: move this to lsp.lua
+	{
+		"L3MON4D3/LuaSnip",
+		version = "v2.3",
+		config = function()
+			local ls = require("luasnip")
+			local s = ls.snippet
+			local t = ls.text_node
+			local i = ls.insert_node
+
+			ls.add_snippets("javascript", {
+				s("express-app", {
+					t({ "const express = require('express');", "\t" }), -- \t is for linebreak
+					t({ "const bodyParser = require('body-parser');", "\t" }),
+					t({ "const axios = require('axios');", "\t" }),
+					t({ "", "\t" }),
+					t({ "const app = express();", "\t" }),
+
+					t({ "app.use(bodyParser.json());", "\t" }),
+					t("app.listen(3000, async () => console.log(`listening on port 3000`));"),
+				}),
+			})
+			-- TODO: add snippets for get and post paths
+			ls.add_snippets("javascript", {
+				s("get-express", {
+					t({ "app.get('/n', async (req,res) => {});" }),
+				}),
+			})
+
+			ls.add_snippets("javascript", {
+				s("post-express", {
+					t({ "app.post('/n', async (req,res) => {});" }),
+				}),
+			})
+		end,
+	},
+	-- TODO: move this to lsp.lua
 	{
 		"hrsh7th/nvim-cmp",
 		event = "InsertEnter", -- load plugin just before entering insert mode
@@ -10,7 +45,6 @@ return {
 			"hrsh7th/cmp-cmdline", -- source for command line completions
 			"saadparwaiz1/cmp_luasnip", -- lua snippets
 			"rafamadriz/friendly-snippets",
-			"L3MON4D3/LuaSnip", -- Snippet engine
 			"onsails/lspkind.nvim", -- pictograms
 		},
 
@@ -41,8 +75,8 @@ return {
 				mapping = cmp.mapping.preset.insert({
 					["<C-n"] = cmp.mapping.select_next_item(),
 					["<C-p"] = cmp.mapping.select_prev_item(),
-					["<C-u>"] = cmp.mapping.scroll_docs(-4),
-					["<C-d>"] = cmp.mapping.scroll_docs(4),
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
@@ -65,7 +99,6 @@ return {
 					{ name = "buffer" },
 				},
 			})
-
 
 			-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 			cmp.setup.cmdline(":", {
