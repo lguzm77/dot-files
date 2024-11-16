16e9ad0319ff021ca8f49e8fb1f7d35da864d3de
lguzm77 <lguzmm77@icloud.com>
2024-11-16 11:30:26 -0500

    Add workspaces to wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,6 +3,7 @@ local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
+local projects = require("projects")
 
 -- config.font = wezterm.font("Dank Mono", { weight = "DemiBold", stretch = "Expanded" })
 config.font = wezterm.font("JetBrains Mono")
@@ -14,7 +15,7 @@ config.default_cursor_style = "BlinkingBar"
 config.enable_tab_bar = true -- modify title bars
 config.window_frame = {
 	font = wezterm.font({ family = "JetBrains Mono", weight = "Bold" }),
-  font_size = 11
+	font_size = 11,
 }
 
 config.window_decorations = "RESIZE"
@@ -32,12 +33,74 @@ config.macos_window_background_blur = 30
 
 config.color_scheme = "rose-pine-moon"
 
+-- configure tab line
+local function segments_for_right_status(window)
+	return {
+		window:active_workspace(),
+		wezterm.strftime("%a %b %-d %H:%M"),
+		wezterm.hostname(),
+	}
+end
+
+wezterm.on("update-status", function(window, _)
+	local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+	local segments = segments_for_right_status(window)
+
+	local color_scheme = window:effective_config().resolved_palette
+	-- Note the use of wezterm.color.parse here, this returns
+	-- a Color object, which comes with functionality for lightening
+	-- or darkening the colour (amongst other things).
+	local bg = wezterm.color.parse(color_scheme.background)
+	local fg = color_scheme.foreground
+
+	-- Each powerline segment is going to be coloured progressively
+	-- darker/lighter depending on whether we're on a dark/light colour
+	-- scheme. Let's establish the "from" and "to" bounds of our gradient.
+	local gradient_to, gradient_from = bg
+	if appearance.is_dark() then
+		gradient_from = gradient_to:lighten(0.2)
+	else
+		gradient_from = gradient_to:darken(0.2)
+	end
+
+	-- Yes, WezTerm supports creating gradients, because why not?! Although
+	-- they'd usually be used for setting high fidelity gradients on your terminal's
+	-- background, we'll use them here to give us a sample of the powerline segment
+	-- colours we need.
+	local gradient = wezterm.color.gradient(
+		{
+			orientation = "Horizontal",
+			colors = { gradient_from, gradient_to },
+		},
+		#segments -- only gives us as many colours as we have segments.
+	)
+
+	-- We'll build up the elements to send to wezterm.format in this table.
+	local elements = {}
+
+	for i, seg in ipairs(segments) do
+		local is_first = i == 1
+
+		if is_first then
+			table.insert(elements, { Background = { Color = "none" } })
+		end
+		table.insert(elements, { Foreground = { Color = gradient[i] } })
+		table.insert(elements, { Text = SOLID_LEFT_ARROW })
+
+		table.insert(elements, { Foreground = { Color = fg } })
+		table.insert(elements, { Background = { Color = gradient[i] } })
+		table.insert(elements, { Text = " " .. seg .. " " })
+	end
+
+	window:set_right_status(wezterm.format(elements))
+end)
+
 local function is_vim(pane)
 	-- this is set by the plugin, and unset on ExitPre in Neovim
 	return pane:get_user_vars().IS_NVIM == "true"
 end
 
--- config borrowed from 
+-- config borrowed from
 -- https://github.com/mrjones2014/smart-splits.nvim
 local direction_keys = {
 	h = "Left",
@@ -118,13 +181,13 @@ config.keys = {
 	},
 	-- move between tabs
 	{
-		key = "n",
+		key = "o",
 		mods = "LEADER",
 		action = wezterm.action.ActivateTabRelative(1),
 	},
 	{
 
-		key = "p",
+		key = "i",
 		mods = "LEADER",
 		action = wezterm.action.ActivateTabRelative(-1),
 	},
@@ -137,6 +200,18 @@ config.keys = {
 		key = "w",
 		mods = "LEADER",
 		action = wezterm.action.CloseCurrentTab({ confirm = true }),
+	},
+	{
+		key = "p",
+		mods = "LEADER",
+		-- Present in to our project picker
+		action = projects.choose_project(),
+	},
+	{
+		key = "f",
+		mods = "LEADER",
+		-- Present a list of existing workspaces
+		action = wezterm.action.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }),
 	},
 }
 
