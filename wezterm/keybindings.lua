31e76304311f9e9057a9548d1a86ca6db57207b6
lguzm77 <lguzmm77@icloud.com>
2024-11-17 09:22:07 -0500

    Separate wezterm config into different files

---
+++ wezterm/keybindings.lua
@@ -0,0 +1,134 @@
+local wezterm = require("wezterm")
+
+local projects = require("projects")
+
+local module = {}
+
+local function is_vim(pane)
+	-- this is set by the plugin, and unset on ExitPre in Neovim
+	return pane:get_user_vars().IS_NVIM == "true"
+end
+
+-- config borrowed from
+-- https://github.com/mrjones2014/smart-splits.nvim
+local direction_keys = {
+	h = "Left",
+	j = "Down",
+	k = "Up",
+	l = "Right",
+}
+
+local function split_nav(resize_or_move, key)
+	return {
+		key = key,
+		mods = resize_or_move == "resize" and "META" or "CTRL",
+		action = wezterm.action_callback(function(win, pane)
+			if is_vim(pane) then
+				-- pass the keys through to vim/nvim
+				win:perform_action({
+					SendKey = { key = key, mods = resize_or_move == "resize" and "META" or "CTRL" },
+				}, pane)
+			else
+				if resize_or_move == "resize" then
+					win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+				else
+					win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+				end
+			end
+		end),
+	}
+end
+
+function module.set_up_keybindings(config)
+	config.leader = { key = "s", mods = "CTRL", timeout_milliseconds = 1000 }
+	config.keys = {
+		-- splitting
+		{
+			mods = "LEADER",
+			key = "-",
+			action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+		},
+		{
+			mods = "LEADER",
+			key = "=",
+			action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+		},
+		{
+			--maximize a window
+			mods = "LEADER",
+			key = "m",
+			action = wezterm.action.TogglePaneZoomState,
+		},
+		-- rotate panes
+		{
+			mods = "LEADER",
+			key = "Space",
+			action = wezterm.action.RotatePanes("Clockwise"),
+		},
+		-- show the pane selection mode, but have it swap the active and selected panes
+		{
+			mods = "LEADER",
+			key = "0",
+			action = wezterm.action.PaneSelect({
+				mode = "SwapWithActive",
+			}),
+		},
+		-- move between split panes
+		split_nav("move", "h"),
+		split_nav("move", "j"),
+		split_nav("move", "k"),
+		split_nav("move", "l"),
+		-- resize panes
+		split_nav("resize", "h"),
+		split_nav("resize", "j"),
+		split_nav("resize", "k"),
+		split_nav("resize", "l"),
+		{
+			key = "t",
+			mods = "LEADER",
+			action = wezterm.action.ShowTabNavigator,
+		},
+		-- Spawn a new tab
+		{
+			key = "k",
+			mods = "LEADER",
+			action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+		},
+		-- move between tabs
+		{
+			key = "o",
+			mods = "LEADER",
+			action = wezterm.action.ActivateTabRelative(1),
+		},
+		{
+
+			key = "i",
+			mods = "LEADER",
+			action = wezterm.action.ActivateTabRelative(-1),
+		},
+		{
+			key = "x",
+			mods = "LEADER",
+			action = wezterm.action.CloseCurrentPane({ confirm = true }),
+		},
+		{
+			key = "w",
+			mods = "LEADER",
+			action = wezterm.action.CloseCurrentTab({ confirm = true }),
+		},
+		{
+			key = "p",
+			mods = "LEADER",
+			-- Present in to our project picker
+			action = projects.choose_project(),
+		},
+		{
+			key = "f",
+			mods = "LEADER",
+			-- Present a list of existing workspaces
+			action = wezterm.action.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }),
+		},
+	}
+end
+
+return module
