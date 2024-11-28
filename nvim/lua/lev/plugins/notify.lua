d98e87dba605437d021f0c76a615edccd3c0e76f
lguzm77 <lguzmm77@icloud.com>
2024-11-28 09:21:48 -0500

    Delete notify

--- nvim/lua/lev/plugins/notify.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"rcarriga/nvim-notify",
-  event = "VeryLazy",
-	config = function()
-		local notify = require("notify")
-		notify.setup({ top_down = true, width = 30 })
-		vim.notify = notify
-	end,
-}
