36182f1ca3a96b0a5c7c17e1347bc2887c41bf7f
lguzm77 <lguzmm77@icloud.com>
2024-11-29 10:25:54 -0500

    Tweak kanagawa

--- nvim/lua/lev/plugins/theme.lua
+++ nvim/lua/lev/plugins/theme.lua
@@ -1,131 +1,48 @@
 return {
-	-- TODO: make themes dynamic
-	-- {
-	-- 	"folke/tokyonight.nvim",
-	-- 	lazy = false,
-	-- 	enable = false,
-	-- 	priority = 1000,
-	-- 	config = function()
-	-- 		require("tokyonight").setup({
-	-- 			style = "storm",
-	-- 			dim_inactive = true,
-	-- 			lualine_bold = true,
-	-- 		})
-	-- 		local number_color = "#7aa2f7"
-	-- 		local number_line_color = "#CEB25E"
-	-- 		vim.cmd("colorscheme tokyonight")
-	-- 		vim.api.nvim_set_hl(0, "LineNrAbove", { fg = number_color, bold = true })
-	-- 		vim.api.nvim_set_hl(0, "LineNr", { fg = number_line_color, bold = true })
-	-- 		vim.api.nvim_set_hl(0, "LineNrBelow", { fg = number_color, bold = true })
-	-- 	end,
-	-- },
-	-- {
-	-- 	"eldritch-theme/eldritch.nvim",
-	-- 	lazy = false,
-	-- 	priority = 1000,
-	-- 	config = function()
-	-- 		require("eldritch").setup({
-	-- 			transparent = true,
-	-- 			dim_inactive = false,
-	-- 			styles = {
-	-- 				floats = "transparent",
-	-- 				sidebars = "transparent",
-	-- 			},
-	-- 		})
-	-- 		vim.cmd("colorscheme eldritch")
-	-- 	end,
-	-- },
 	{
 		"rebelot/kanagawa.nvim",
 		config = function()
 			require("kanagawa").setup({
 				transparent = true,
+				overrides = function(colors)
+					local theme = colors.theme
+					return {
+						TelescopeTitle = { fg = theme.ui.special, bold = true },
+						TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+						TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+						TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+						TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+						TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+						TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+
+						NormalFloat = { bg = "none" },
+						FloatBorder = { bg = "none" },
+						FloatTitle = { bg = "none" },
+
+						-- Save an hlgroup with dark background and dimmed foreground
+						-- so that you can use it where your still want darker windows.
+						-- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+						NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+
+						-- Popular plugins that open floats will link to NormalFloat by default;
+						-- set their background accordingly if you wish to keep them dark and borderless
+						LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+						MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+					}
+				end,
+				colors = {
+					theme = {
+						all = {
+							ui = {
+								bg_gutter = "none",
+							},
+						},
+					},
+				},
 			})
 			vim.cmd("colorscheme kanagawa")
 		end,
 	},
-	-- {
-	-- 	"ribru17/bamboo.nvim",
-	-- 	lazy = false,
-	-- 	priority = 1000,
-	-- 	config = function()
-	-- 		require("bamboo").setup({
-	-- 			-- optional configuration here
-	-- 			-- dim_inactive = true,
-	-- 			transparent = true,
-	-- 		})
-	-- 		require("bamboo").load()
-	-- 	end,
-	-- },
-	--
-	-- {
-	-- 	"rose-pine/neovim",
-	--    enabled = false,
-	-- 	name = "rose-pine",
-	-- 	config = function()
-	-- 		require("rose-pine").setup({
-	-- 			variant = "auto", -- auto, main, moon, or dawn
-	-- 			dark_variant = "moon", -- main, moon, or dawn
-	-- 			dim_inactive_windows = false,
-	-- 			extend_background_behind_borders = true,
-	--
-	-- 			enable = {
-	-- 				terminal = true,
-	-- 				legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
-	-- 				migrations = true, -- Handle deprecated options automatically
-	-- 			},
-	--
-	-- 			styles = {
-	-- 				bold = true,
-	-- 				italic = true,
-	-- 				transparency = true,
-	-- 			},
-	--
-	-- 			-- Transparent telescope
-	-- 			highlight_groups = {
-	-- 				TelescopeBorder = { fg = "highlight_high", bg = "none" },
-	-- 				TelescopeNormal = { bg = "none" },
-	-- 				TelescopePromptNormal = { bg = "base" },
-	-- 				TelescopeResultsNormal = { fg = "subtle", bg = "none" },
-	-- 				TelescopeSelection = { fg = "text", bg = "base" },
-	-- 				TelescopeSelectionCaret = { fg = "rose", bg = "rose" },
-	-- 			},
-	--
-	-- 			groups = {
-	-- 				border = "muted",
-	-- 				link = "iris",
-	-- 				panel = "surface",
-	--
-	-- 				error = "love",
-	-- 				hint = "iris",
-	-- 				info = "foam",
-	-- 				note = "pine",
-	-- 				todo = "rose",
-	-- 				warn = "gold",
-	--
-	-- 				git_add = "foam",
-	-- 				git_change = "rose",
-	-- 				git_delete = "love",
-	-- 				git_dirty = "rose",
-	-- 				git_ignore = "muted",
-	-- 				git_merge = "iris",
-	-- 				git_rename = "pine",
-	-- 				git_stage = "iris",
-	-- 				git_text = "rose",
-	-- 				git_untracked = "subtle",
-	--
-	-- 				h1 = "iris",
-	-- 				h2 = "foam",
-	-- 				h3 = "rose",
-	-- 				h4 = "gold",
-	-- 				h5 = "pine",
-	-- 				h6 = "foam",
-	-- 			},
-	-- 		})
-	--
-	-- 		vim.cmd("colorscheme rose-pine")
-	-- 	end,
-	-- },
 	{
 		"nvim-lualine/lualine.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons", "AndreM222/copilot-lualine", "folke/noice.nvim" },
