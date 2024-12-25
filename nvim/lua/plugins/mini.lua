b32027b90b6ea6abeee71567ab591466684c152d
lguzm77 <lguzmm77@icloud.com>
2024-12-25 10:10:58 -0500

    Enable pairs and surround in mini.nvim

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -6,6 +6,13 @@ return {
 		config = function()
 			local statusline = require("mini.statusline")
 			statusline.setup({ use_icons = true })
+
+      local pairs = require("mini.pairs")
+      pairs.setup() 
+
+      local surround = require("mini.surround") -- surround actions
+      surround.setup()
+
 		end,
 	},
 }
