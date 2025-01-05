bfbb0a0f1d873d1176ae02dd3c80c64a636a3b4f
lguzm77 <lguzmm77@icloud.com>
2025-01-05 07:56:38 -0500

    Delete comments plugin and enable mini.comment

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,18 +1,20 @@
 return {
 	{
 		"echasnovski/mini.nvim",
-    event = "VeryLazy",
+		event = "VeryLazy",
 		enabled = true,
 		config = function()
 			local statusline = require("mini.statusline")
 			statusline.setup({ use_icons = true })
 
-      local pairs = require("mini.pairs")
-      pairs.setup() 
+			local pairs = require("mini.pairs")
+			pairs.setup()
 
-      local surround = require("mini.surround") -- surround actions
-      surround.setup()
+			local surround = require("mini.surround") -- surround actions
+			surround.setup()
 
+			local comment = require("mini.comment")
+			comment.setup()
 		end,
 	},
 }
