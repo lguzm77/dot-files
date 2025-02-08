aa8fed3ff63a8620f7b98c0cdfcf2138bb52ba26
lguzm77 <lguzmm77@icloud.com>
2025-02-07 22:42:42 -0500

    Remove dead code

--- wezterm/appearance.lua
+++ wezterm/appearance.lua
@@ -3,21 +3,6 @@ local wezterm = require("wezterm")
 -- Define a lua table to hold _our_ module's functions
 local module = {}
 
--- Returns a bool based on whether the host operating system's
--- appearance is light or dark.
-local function is_dark()
-	-- wezterm.gui is not always available, depending on what
-	-- environment wezterm is operating in. Just return true
-	-- if it's not defined.
-	if wezterm.gui then
-		-- Some systems report appearance like "Dark High Contrast"
-		-- so let's just look for the string "Dark" and if we find
-		-- it assume appearance is dark.
-		return wezterm.gui.get_appearance():find("Dark")
-	end
-	return true
-end
-
 function module.set_up_appearance(config)
 		config.color_scheme = "Kanagawa (Gogh)"
 end
