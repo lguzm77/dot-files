f4a2568458b81f88a3f95d66d79d51b9fccf9fca
lguzm77 <lguzmm77@icloud.com>
2025-02-11 08:22:13 -0500

    First autocmd!

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,1 +1,8 @@
 -- For custom callbacks
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
