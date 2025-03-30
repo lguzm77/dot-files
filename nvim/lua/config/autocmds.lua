c69da853ceb424b4c847d1f5bd483cb9324e0b55
lguzm77 <lguzmm77@icloud.com>
2025-03-30 09:11:53 -0400

    Create autocommands to attach/detach lsp clients based on buffer state

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,25 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank()
 	end,
 })
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+	desc = "Only attach lsp client to open buffers",
+	callback = function()
+		local bufnr = vim.api.nvim_get_current_buf()
+		local clients = vim.lsp.get_active_clients()
+		for _, client in ipairs(clients) do
+			vim.lsp.buf_attach_client(bufnr, client.id)
+		end
+	end,
+})
+
+vim.api.nvim_create_autocmd("BufUnload", {
+  desc = "Detach lsp client when a buffer is closed",
+	callback = function()
+		local bufnr = vim.api.nvim_get_current_buf()
+		local clients = vim.lsp.get_active_clients()
+		for _, client in ipairs(clients) do
+			vim.lsp.buf_detach_client(bufnr, client.id)
+		end
+	end,
+})
