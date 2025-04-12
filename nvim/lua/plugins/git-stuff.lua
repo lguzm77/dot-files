b356c718e5b86decdb40f5f9360b01146ffb609c
lguzm77 <lguzmm77@icloud.com>
2025-04-12 08:49:01 -0400

    Remove GBrowse

--- nvim/lua/plugins/git-stuff.lua
+++ nvim/lua/plugins/git-stuff.lua
@@ -18,14 +18,6 @@ return {
 		dependencies = { "tpope/vim-rhubarb" },
 		event = "BufRead",
 		config = function()
-			-- Custom :Browse command
-			vim.api.nvim_create_user_command("Browse", function(opts)
-				-- xdg-open for linux, open for mac
-				-- TODO: Can you make this function execute the correct command based on the operation system?
-				vim.fn.system({ "open", opts.fargs[1] })
-			end, { nargs = 1 })
-
-			vim.keymap.set({ "n", "v" }, "<leader>op", ":GBrowse<CR>", {})
 			vim.keymap.set("n", "<leader>df", "<cmd>:Gvdiffsplit!<cr>", { desc = "Git conflict resolution" })
 		end,
 	},
