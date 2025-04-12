931f2e6226756dbd9c3e0848800fb6a6a36acdae
lguzm77 <lguzmm77@icloud.com>
2025-04-12 09:33:49 -0400

    Apply linting and add some keybinding

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,228 +1,269 @@
 return {
-	"folke/snacks.nvim",
-	priority = 1000,
-	lazy = false,
-	---@type snacks.Config
-	opts = {
-		bigfile = { enabled = true },
-		toggle = { enabled = true },
-		input = { enabled = true },
-		picker = { enabled = true },
-		dashboard = {
-			enabled = true,
-			preset = {
-				keys = {
-					{ icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-					-- { icon = " ", key = "s", desc = "Restore Previous CWD Session", action = ":SessionRestore" },
-					{
-						icon = " ",
-						key = "g",
-						desc = "Find Text",
-						action = ":lua Snacks.dashboard.pick('live_grep')",
-					},
-					{
-						icon = " ",
-						key = "r",
-						desc = "Recent Files",
-						action = ":lua Snacks.dashboard.pick('oldfiles')",
-					},
-					{
-						icon = " ",
-						key = "c",
-						desc = "Config",
-						action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
-					},
-					{
-						icon = "󰒲 ",
-						key = "L",
-						desc = "Lazy",
-						action = ":Lazy",
-						enabled = package.loaded.lazy ~= nil,
-					},
-					{ icon = "󰣪", key = "M", desc = "Mason", action = ":Mason" },
-					{ icon = " ", key = "O", desc = "List PRs for Current Repo", action = ":Octo pr list" },
-					{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
-				},
-			},
-			sections = {
-				{ section = "header" },
-				{
-					section = "terminal",
-					cmd = "pokemon-colorscripts -r --no-title; sleep .1",
-					random = 10,
-					pane = 2,
-					indent = 4,
-					height = 30,
-				},
-				{ icon = " ", title = "Keymaps", section = "keys", gap = 0.1, indent = 2, padding = 1 },
-				{ icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-				{ icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
-				{ section = "startup" },
-			},
-		},
-		notifier = {
-			enabled = true,
-			timeout = 3000,
-			top_down = false,
-			margin = { top = 0, right = 1, bottom = 1 },
-			style = "fancy",
-		},
-		lazygit = { enabled = true, dependencies = {
-			"nvim-lua/plenary.nvim",
-		} },
-		quickfile = { enabled = true },
-		statuscolumn = { enabled = true },
-		words = { enabled = true },
-		styles = {
-			input = {
-				relative = "cursor",
-			},
-			notification = {
-				wo = { wrap = true }, -- Wrap notifications
-			},
-		},
-	},
-	keys = {
-		-- Top Pickers & Explorer
-		{
-			"<leader><space>",
-			function()
-				Snacks.picker.smart()
-			end,
-			desc = "Smart Find Files",
-		},
-		{
-			"<leader>/",
-			function()
-				Snacks.picker.grep()
-			end,
-			desc = "Grep",
-		},
-		{
-			"<leader>:",
-			function()
-				Snacks.picker.command_history()
-			end,
-			desc = "Command History",
-		},
-		{
-			"<leader>n",
-			function()
-				Snacks.picker.notifications()
-			end,
-			desc = "Notification History",
-		},
-		-- {
-		-- 	"<leader>e",
-		-- 	function()
-		-- 		Snacks.explorer()
-		-- 	end,
-		-- 	desc = "File Explorer",
-		-- },
-		--
-		-- find
-		{
-			"<leader>fb",
-			function()
-				Snacks.picker.buffers()
-			end,
-			desc = "Buffers",
-		},
-		{
-			"<leader>fc",
-			function()
-				Snacks.picker.files({ cwd = vim.fn.stdpath("config") })
-			end,
-			desc = "Find Config File",
-		},
-		{
-			"<leader>ff",
-			function()
-				Snacks.picker.files()
-			end,
-			desc = "Find Files",
-		},
-		{
-			"<leader>fg",
-			function()
-				Snacks.picker.git_files()
-			end,
-			desc = "Find Git Files",
-		},
-		{
-			"<leader>fp",
-			function()
-				Snacks.picker.projects()
-			end,
-			desc = "Projects",
-		},
-		{
-			"<leader>fr",
-			function()
-				Snacks.picker.recent()
-			end,
-			desc = "Recent",
-		},
-		--others
-		{
-			"<leader>z",
-			function()
-				Snacks.zen()
-			end,
-			desc = "Toggle Zen Mode",
-		},
-		{
-			"<leader>gB",
-			function()
-				Snacks.gitbrowse()
-			end,
-			desc = "Git Browse",
-			mode = { "n", "v" },
-		},
-		{
-			"<leader>gg",
-			function()
-				Snacks.lazygit()
-			end,
-			desc = "Lazygit",
-		},
-	},
-	init = function()
-		vim.api.nvim_create_autocmd("User", {
-			pattern = "VeryLazy",
-			callback = function()
-				-- Setup some globals for debugging (lazy-loaded)
-				_G.dd = function(...)
-					Snacks.debug.inspect(...)
-				end
-				_G.bt = function()
-					Snacks.debug.backtrace()
-				end
-				vim.print = _G.dd -- Override print to use snacks for `:=` command
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    bigfile = { enabled = true },
+    toggle = { enabled = true },
+    input = { enabled = true },
+    picker = { enabled = true },
+    dashboard = {
+      enabled = true,
+      preset = {
+        keys = {
+          {
+            icon = " ",
+            key = "f",
+            desc = "Find File",
+            action = ":lua Snacks.dashboard.pick('files')",
+          },
+          -- { icon = " ", key = "s", desc = "Restore Previous CWD Session", action = ":SessionRestore" },
+          {
+            icon = " ",
+            key = "g",
+            desc = "Find Text",
+            action = ":lua Snacks.dashboard.pick('live_grep')",
+          },
+          {
+            icon = " ",
+            key = "r",
+            desc = "Recent Files",
+            action = ":lua Snacks.dashboard.pick('oldfiles')",
+          },
+          {
+            icon = " ",
+            key = "c",
+            desc = "Config",
+            action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+          },
+          {
+            icon = "󰒲 ",
+            key = "L",
+            desc = "Lazy",
+            action = ":Lazy",
+            enabled = package.loaded.lazy ~= nil,
+          },
+          { icon = "󰣪", key = "M", desc = "Mason", action = ":Mason" },
+          {
+            icon = " ",
+            key = "O",
+            desc = "List PRs for Current Repo",
+            action = ":Octo pr list",
+          },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+      },
+      sections = {
+        { section = "header" },
+        {
+          section = "terminal",
+          cmd = "pokemon-colorscripts -r --no-title; sleep .1",
+          random = 10,
+          pane = 2,
+          indent = 4,
+          height = 30,
+        },
+        {
+          icon = " ",
+          title = "Keymaps",
+          section = "keys",
+          gap = 0.1,
+          indent = 2,
+          padding = 1,
+        },
+        {
+          icon = " ",
+          title = "Recent Files",
+          section = "recent_files",
+          indent = 2,
+          padding = 1,
+        },
+        {
+          icon = " ",
+          title = "Projects",
+          section = "projects",
+          indent = 2,
+          padding = 1,
+        },
+        { section = "startup" },
+      },
+    },
+    notifier = {
+      enabled = true,
+      timeout = 3000,
+      top_down = false,
+      margin = { top = 0, right = 1, bottom = 1 },
+      style = "fancy",
+    },
+    lazygit = {
+      enabled = true,
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+      },
+    },
+    quickfile = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+    styles = {
+      input = {
+        relative = "cursor",
+      },
+      notification = {
+        wo = { wrap = true }, -- Wrap notifications
+      },
+    },
+  },
+  keys = {
+    -- Top Pickers & Explorer
+    {
+      "<leader><space>",
+      function()
+        Snacks.picker.smart()
+      end,
+      desc = "Smart Find Files",
+    },
+    {
+      "<leader>/",
+      function()
+        Snacks.picker.grep()
+      end,
+      desc = "Grep",
+    },
+    {
+      "<leader>:",
+      function()
+        Snacks.picker.command_history()
+      end,
+      desc = "Command History",
+    },
+    {
+      "<leader>n",
+      function()
+        Snacks.picker.notifications()
+      end,
+      desc = "Notification History",
+    },
+    -- {
+    -- 	"<leader>e",
+    -- 	function()
+    -- 		Snacks.explorer()
+    -- 	end,
+    -- 	desc = "File Explorer",
+    -- },
+    --
+    -- find
+    {
+      "<leader>fb",
+      function()
+        Snacks.picker.buffers()
+      end,
+      desc = "Buffers",
+    },
+    {
+      "<leader>fc",
+      function()
+        Snacks.picker.files({ cwd = vim.fn.stdpath "config" })
+      end,
+      desc = "Find Config File",
+    },
+    {
+      "<leader>fg",
+      function()
+        Snacks.picker.git_files()
+      end,
+      desc = "Find Git Files",
+    },
+    {
+      "<leader>fp",
+      function()
+        Snacks.picker.projects()
+      end,
+      desc = "Projects",
+    },
+    {
+      "<leader>fr",
+      function()
+        Snacks.picker.recent()
+      end,
+      desc = "Recent",
+    },
+    -- searches
+    {
+      "<leader>sk",
+      function()
+        Snacks.picker.keymaps()
+      end,
+      desc = "Keymaps",
+    },
+    --others
+    {
+      "<leader>z",
+      function()
+        Snacks.zen()
+      end,
+      desc = "Toggle Zen Mode",
+    },
+    {
+      "<leader>gB",
+      function()
+        Snacks.gitbrowse()
+      end,
+      desc = "Git Browse",
+      mode = { "n", "v" },
+    },
+    {
+      "<leader>gg",
+      function()
+        Snacks.lazygit()
+      end,
+      desc = "Lazygit",
+    },
+  },
+  init = function()
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "VeryLazy",
+      callback = function()
+        -- Setup some globals for debugging (lazy-loaded)
+        _G.dd = function(...)
+          Snacks.debug.inspect(...)
+        end
+        _G.bt = function()
+          Snacks.debug.backtrace()
+        end
+        vim.print = _G.dd -- Override print to use snacks for `:=` command
 
-				-- Create some toggle mappings
-				Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
-				Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
-				Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
-				Snacks.toggle.diagnostics():map("<leader>ud")
-				Snacks.toggle.line_number():map("<leader>ul")
-				Snacks.toggle
-					.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
-					:map("<leader>uc")
-				Snacks.toggle.treesitter():map("<leader>uT")
-				Snacks.toggle
-					.option("background", { off = "light", on = "dark", name = "Dark Background" })
-					:map("<leader>ub")
-				Snacks.toggle.inlay_hints():map("<leader>uh")
-			end,
-		})
+        -- Create some toggle mappings
+        Snacks.toggle.option("spell", { name = "Spelling" }):map "<leader>us"
+        Snacks.toggle.option("wrap", { name = "Wrap" }):map "<leader>uw"
+        Snacks.toggle
+          .option("relativenumber", { name = "Relative Number" })
+          :map "<leader>uL"
+        Snacks.toggle.diagnostics():map "<leader>ud"
+        Snacks.toggle.line_number():map "<leader>ul"
+        Snacks.toggle
+          .option(
+            "conceallevel",
+            { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }
+          )
+          :map "<leader>uc"
+        Snacks.toggle.treesitter():map "<leader>uT"
+        Snacks.toggle
+          .option(
+            "background",
+            { off = "light", on = "dark", name = "Dark Background" }
+          )
+          :map "<leader>ub"
+        Snacks.toggle.inlay_hints():map "<leader>uh"
+      end,
+    })
 
-		-- Mini.files autocmd
-		vim.api.nvim_create_autocmd("User", {
-			pattern = "MiniFilesActionRename",
-			callback = function(event)
-				Snacks.rename.on_rename_file(event.data.from, event.data.to)
-			end,
-		})
-	end,
+    -- Mini.files autocmd
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "MiniFilesActionRename",
+      callback = function(event)
+        Snacks.rename.on_rename_file(event.data.from, event.data.to)
+      end,
+    })
+  end,
 }
