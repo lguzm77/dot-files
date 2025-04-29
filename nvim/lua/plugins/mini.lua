95cf0448b6bba951e87576e6e6601d0ee2ff2581
lguzm77 <lguzmm77@icloud.com>
2025-04-29 08:23:31 -0400

    Delete mini.files

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,34 +1,26 @@
 local plugins = {
-	statusline = {
-		use_icons = true,
-	},
-	pairs = {}, -- navigate easier between symbol pairs
-	surround = {}, -- wrap around words
-	comment = {}, -- commenting keybdings
-	sessions = {}, -- session management
-	tabline = {}, -- tabs
-	ai = {}, --text objects
-	operators = {},
-	bracketed = {},
-	files = {
-		windows = {
-			width_preview = math.floor(0.3 * vim.o.columns),
-			preview = true,
-		},
-	}, -- filesystem
+  statusline = {
+    use_icons = true,
+  },
+  pairs = {}, -- navigate easier between symbol pairs
+  surround = {}, -- wrap around words
+  comment = {}, -- commenting keybdings
+  sessions = {}, -- session management
+  tabline = {}, -- tabs
+  ai = {}, --text objects
+  operators = {},
+  bracketed = {},
 }
 
 return {
-	{
-		"echasnovski/mini.nvim",
-		event = "VeryLazy",
-		enabled = true,
-		config = function()
-			for name, config in pairs(plugins) do
-				require(string.format("mini.%s", name)).setup(config)
-			end
-
-			vim.keymap.set("n", "<leader>e", ":lua MiniFiles.open()<cr>", { desc = "Open filesystem" })
-		end,
-	},
+  {
+    "echasnovski/mini.nvim",
+    event = "VeryLazy",
+    enabled = true,
+    config = function()
+      for name, config in pairs(plugins) do
+        require(string.format("mini.%s", name)).setup(config)
+      end
+    end,
+  },
 }
