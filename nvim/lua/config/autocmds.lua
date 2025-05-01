599a51dea5c46f0cf6b5a63d075acac873cf76c4
lguzm77 <lguzmm77@icloud.com>
2025-05-01 17:03:35 -0400

    Delete lsp autocommands

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -10,32 +10,3 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   end,
 })
 
-vim.api.nvim_create_autocmd("BufReadPost", {
-  desc = "Only attach lsp client to open buffers",
-  group = vim.api.nvim_create_augroup(
-    "lsp-buffer-management",
-    { clear = true }
-  ),
-  callback = function()
-    local bufnr = vim.api.nvim_get_current_buf()
-    local clients = vim.lsp.get_active_clients()
-    for _, client in ipairs(clients) do
-      vim.lsp.buf_attach_client(bufnr, client.id)
-    end
-  end,
-})
-
-vim.api.nvim_create_autocmd("BufDelete", {
-  desc = "Detach lsp client when a buffer is closed",
-  group = vim.api.nvim_create_augroup(
-    "lsp-buffer-management",
-    { clear = true }
-  ),
-  callback = function()
-    local bufnr = vim.api.nvim_get_current_buf()
-    local clients = vim.lsp.get_active_clients()
-    for _, client in ipairs(clients) do
-      vim.lsp.buf_detach_client(bufnr, client.id)
-    end
-  end,
-})
