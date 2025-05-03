f92647a4cad743ceb1adae0f010b0a599c60a0b3
lguzm77 <lguzmm77@icloud.com>
2025-05-03 13:00:19 -0400

    Remove linting autocommand

--- nvim/lua/plugins/formatting.lua
+++ nvim/lua/plugins/formatting.lua
@@ -1,65 +1,56 @@
 return {
-	{
-		"stevearc/conform.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		config = function()
-			local conform = require("conform")
+  {
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      local conform = require "conform"
 
-			conform.setup({
-				-- install additional formatters via Mason
-				formatters_by_ft = {
-					lua = { "stylua" },
-					javascript = { "prettierd" },
-					typescript = { "prettierd" },
-					yaml = { "prettierd" },
-					yml = { "prettierd" },
-					json = { "prettierd" },
-					csharp = { "csharpier" },
-					["*"] = { "codespell" },
-					go = { "gofumpt" },
-					markdown = { "prettierd" },
-					bash = { "beautysh" },
-					sh = { "shellcheck" },
-					proto = { "buf" },
-				},
-			})
+      conform.setup({
+        -- install additional formatters via Mason
+        formatters_by_ft = {
+          lua = { "stylua" },
+          javascript = { "prettierd" },
+          typescript = { "prettierd" },
+          yaml = { "prettierd" },
+          yml = { "prettierd" },
+          json = { "prettierd" },
+          csharp = { "csharpier" },
+          ["*"] = { "codespell" },
+          go = { "gofumpt" },
+          markdown = { "prettierd" },
+          bash = { "beautysh" },
+          sh = { "shellcheck" },
+          proto = { "buf" },
+        },
+      })
 
-			vim.keymap.set({ "n", "v" }, "<leader>l", function()
-				conform.format({
-					lsp_fallback = true,
-					async = true,
-					timeout_ms = 1000,
-				})
-			end, { desc = "Format file or range (in visual)" })
-		end,
-	},
-	{
-		"mfussenegger/nvim-lint",
-		event = { "BufReadPre", "BufNewFile" },
-		config = function()
-			local lint = require("lint")
+      vim.keymap.set({ "n", "v" }, "<leader>l", function()
+        conform.format({
+          lsp_fallback = true,
+          async = true,
+          timeout_ms = 1000,
+        })
+      end, { desc = "Format file or range (in visual)" })
+    end,
+  },
+  {
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      local lint = require "lint"
 
-			lint.linters_by_ft = {
-				javascript = { "eslint" },
-				typescript = { "eslint" },
-				yaml = { "yamllint" },
-				protobuf = { "buf" },
-				terraform = { "tflint" },
-				["*"] = { "codespell" },
-			}
+      lint.linters_by_ft = {
+        javascript = { "eslint" },
+        typescript = { "eslint" },
+        yaml = { "yamllint" },
+        protobuf = { "buf" },
+        terraform = { "tflint" },
+        ["*"] = { "codespell" },
+      }
 
-			local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-
-			vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-				group = lint_augroup,
-				callback = function()
-					lint.try_lint()
-				end,
-			})
-
-			vim.keymap.set("n", "<leader>ll", function()
-				lint.try_lint()
-			end, { desc = "Trigger linting for current file" })
-		end,
-	},
+      vim.keymap.set("n", "<leader>ll", function()
+        lint.try_lint()
+      end, { desc = "Trigger linting for current file" })
+    end,
+  },
 }
