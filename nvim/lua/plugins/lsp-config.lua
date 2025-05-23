1a7f04c4c973faf38943c3edcb3d8ec8306fba33
lguzm77 <lguzmm77@icloud.com>
2025-05-23 15:15:08 -0400

    Change lsp keybindings to use native functions

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -104,19 +104,19 @@ return {
           local opts = { buffer = ev.buf, silent = true }
 
           opts.desc = "Show LSP references"
-          keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+          keymap.set("n", "gR", vim.lsp.buf.references, opts) -- show definition, references
 
           opts.desc = "Go to declaration"
           keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
 
           opts.desc = "Show LSP definitions"
-          keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>zz", opts) -- show lsp definitions
+          keymap.set("n", "gd", vim.lsp.buf.definition, opts) -- show lsp definitions
 
           opts.desc = "Show LSP implementations"
-          keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+          keymap.set("n", "gi", vim.lsp.buf.implementation, opts) -- show lsp implementations
 
           opts.desc = "Show LSP type definitions"
-          keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+          keymap.set("n", "gt", vim.lsp.buf.type_definition, opts) -- show lsp type definitions
 
           opts.desc = "See available code actions"
           keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
@@ -125,12 +125,7 @@ return {
           keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
 
           opts.desc = "Show buffer diagnostics"
-          keymap.set(
-            "n",
-            "<leader>D",
-            "<cmd>Telescope diagnostics bufnr=0<CR>",
-            opts
-          ) -- show  diagnostics for file
+          keymap.set("n", "<leader>D", vim.diagnostic.get, opts) -- show  diagnostics for file
 
           opts.desc = "Show line diagnostics"
           keymap.set("n", "<leader>dg", vim.diagnostic.open_float, opts) -- show diagnostics for line
