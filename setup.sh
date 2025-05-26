06b2817e6b6b513b1f5556346e034a6af2354b44
lguzm77 <lguzmm77@icloud.com>
2025-05-25 20:17:44 -0400

    Add dependencies command and helper functions

--- setup.sh
+++ setup.sh
@@ -3,7 +3,32 @@
 dependencies(){
   echo "Installing dependencies"
 
+  if ! command -v brew > /dev/null ; then 
+    echo "Homebrew not found, installing"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" # install Homebrew
+  fi 
 
+  # Install all packages 
+  xargs brew install < "$PWD/homebrew/leaves.txt"
+
+  # Casks have a slightly different syntax
+  brew install --cask wezterm 
+  # Install window manager
+}
+
+shell() {
+  echo "Not implemented"
+
+}
+
+editor () {
+  echo "Editor not implemented"
+}
+
+all() {
+  shell
+  dependencies
+  editor
 }
 
 
@@ -11,11 +36,15 @@ case "$1" in
   dependencies) 
     dependencies
     ;;
+  shell)
+    shell
+    ;;
   editor) 
-    echo "editor not implemented"
+    editor
     ;;
   *) 
-    echo "Hello World"
+    all
     ;;
 esac 
+
 
