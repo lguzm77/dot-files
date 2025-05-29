7351056e8aa060f52d70ca1935a3a66dcb28a8a0
lguzm77 <lguzmm77@icloud.com>
2025-05-29 08:18:53 -0400

    Set up pywal in wezterm

--- wezterm/appearance.lua
+++ wezterm/appearance.lua
@@ -1,7 +1,11 @@
 local module = {}
+local wezterm = require("wezterm")
+local HOME = os.getenv("HOME")
 
 function module.set_up_appearance(config)
-		config.color_scheme = "Kanagawa (Gogh)"
+	wezterm.add_to_config_reload_watch_list(HOME .. "/.cache/wal/wezterm-wal.toml")
+	config.color_scheme_dirs = { HOME .. "/.cache/wal/" }
+	config.color_scheme = "wezterm-wal"
 end
 
 return module
