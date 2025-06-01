bc3271e4c8839f1388383b42f5c2fe88e76db0f0
lguzm77 <lguzmm77@icloud.com>
2025-06-01 08:14:37 -0400

    Add tab config colors

--- wezterm/appearance.lua
+++ wezterm/appearance.lua
@@ -6,6 +6,16 @@ function module.set_up_appearance(config)
 	wezterm.add_to_config_reload_watch_list(HOME .. "/.cache/wal/wezterm-wal.toml")
 	config.color_scheme_dirs = { HOME .. "/.cache/wal/" }
 	config.color_scheme = "wezterm-wal"
+
+	-- Tab color configuration
+  -- TODO: change tab bar background
+	config.colors = {
+		tab_bar = {
+			-- The color of the strip that goes along the top of the window
+			-- (does not apply when fancy tab bar is in use)
+			-- background = config.color_schemes['wezterm-wal'].background,
+		},
+	}
 end
 
 return module
