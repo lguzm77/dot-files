db1d58b328ca68d273d68a6d98ca585f37ddd338
Lev Guzman <levguzman@levguzman.local>
2026-07-06 11:09:02 -0700

    Pin down nixpkgs,nix-darwin and home-manager versions

--- nix/flake.nix
+++ nix/flake.nix
@@ -2,13 +2,14 @@
   description = "My nix-darwin configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    # NOTE: versions for nixpkgs, nix-darwin, home-manager should match
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-26.05-darwin";
     nix-darwin = {
-      url = "github:LnL7/nix-darwin/master";
+      url = "github:LnL7/nix-darwin/nix-darwin-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -21,14 +22,14 @@
           ./hosts/mbp/configuration.nix
           home-manager.darwinModules.home-manager
           {
-            users.users.yourname = {
+            users.users.levguzman = {
               name = "levguzman";
               home = "/Users/levguzman";
             };
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.backupFileExtension = "backup";
-            home-manager.users.yourname = import ./home/home.nix;
+            home-manager.users.levguzman = import ./home/home.nix;
           }
         ];
       };
